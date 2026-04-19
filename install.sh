6157ecd092604d487e9e3f938ef67e279ce4be23
Toby Chappell <Toby.Chappell@maxcontact.com>
2026-04-19 10:31:35 +0100

    Fix: create ~/.ssh dir before symlinking ssh config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf "$DOTFILES/.gitconfig" ~/.gitconfig
 ln -sf "$DOTFILES/.gitconfig-maxcontact" ~/.gitconfig-maxcontact
 ln -sf "$DOTFILES/.tmux.conf" ~/.tmux.conf
 ln -sf "$DOTFILES/.zshrc" ~/.zshrc
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
 ln -sf "$DOTFILES/.ssh/config" ~/.ssh/config
 
 chmod +x "$DOTFILES/bootstrap.sh"
